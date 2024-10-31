f84524059f1fcdf0480a525d4145bc6a26a0beb2
Sam Wyss <sjwyss@outlook.com>
2024-10-30 21:31:38 -0400

    filled out main.rs skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+//! driver binary crate
+//!
+//! picrs build target
+
+use anyhow::Result;
+
+/// main driver function
+///
+/// # Arguments
+///
+/// # Returns
+/// `Result<(), anyhow::Error>`
+///
+/// # Errors
+fn main() -> Result<(), anyhow::Error> {
     println!("Hello, world!");
+
+    Ok(())
 }
