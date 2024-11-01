965e286ca805d2eb2e08b42610b5f31c0d0a1fac
Sam Wyss <sjwyss@outlook.com>
2024-10-31 21:04:57 -0400

    tested field module

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,3 +1,5 @@
+use crate::field::{ScalarField, VectorField};
+
 pub struct Model {}
 
 impl Model {
@@ -5,5 +7,19 @@ impl Model {
         Ok(Model {})
     }
 
-    pub fn run(&mut self) {}
+    pub fn run(&mut self) -> Result<(), anyhow::Error> {
+        let mut field: ScalarField<f64> = ScalarField::new(2, 2, 2)?;
+        field[(1, 1, 1)] = 1000.0;
+        field[(0, 0, 0)] = 1000.0;
+
+        println!("{}", field);
+
+        let mut vec_field: VectorField<f64> = VectorField::new(2, 2, 2)?;
+        vec_field[(1, 1, 1, 0)] = 1000.0;
+        vec_field[(0, 0, 0, 2)] = 1000.0;
+
+        println!("{}", vec_field);
+
+        Ok(())
+    }
 }
