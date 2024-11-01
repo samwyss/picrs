9798fa23d57cbfea9883fd161efb34e85d931fa4
Sam Wyss <sjwyss@outlook.com>
2024-11-01 16:51:12 -0400

    removed vector fields temporarily

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,4 +1,4 @@
-use crate::world::{CoordinateTriplet, ScalarField, VectorField};
+use crate::world::{CoordinateTriplet, ScalarField};
 
 pub struct Model {}
 
@@ -8,20 +8,15 @@ impl Model {
     }
 
     pub fn run(&mut self) -> Result<(), anyhow::Error> {
-        let coordinates: CoordinateTriplet<usize> = CoordinateTriplet::new(2, 2, 2)?;
+        let coordinates: CoordinateTriplet<usize> = CoordinateTriplet::new(1, 1, 1)?;
 
         let mut field: ScalarField<f64> = ScalarField::new(coordinates)?;
-        field[(1, 1, 1)] = 50.0;
-        field[(0, 0, 0)] = 1000.0;
+
+        field[(0, 0, 0)] = 1.0;
+
+        field *= 2.0;
 
         println!("{}", field);
-
-        let coordinates: CoordinateTriplet<usize> = CoordinateTriplet::new(2, 2, 2)?;
-        let mut vec_field: VectorField<f64> = VectorField::new(coordinates)?;
-        vec_field[(1, 1, 1)].x = 1000.0;
-        vec_field[(0, 0, 0)].z = 50.0;
-
-        println!("{}", vec_field);
 
         Ok(())
     }
