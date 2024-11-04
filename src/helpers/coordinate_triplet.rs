cf59cac33f1a39106883c5fcbbe49ab722c39b57
Sam Wyss <sjwyss@outlook.com>
2024-11-03 22:23:33 -0500

    added simple test for new()

--- src/helpers/coordinate_triplet.rs
+++ src/helpers/coordinate_triplet.rs
@@ -3,7 +3,7 @@ use std::fmt::{Display, Formatter};
 /// `CoordinateTriplet` struct
 ///
 /// represents generic data that by nature has (x, y, z) components
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct CoordinateTriplet<T> {
     /// x component
     pub x: T,
@@ -41,5 +41,22 @@ impl<T: Display> Display for CoordinateTriplet<T> {
 
 #[cfg(test)]
 mod tests {
-    
+    use crate::helpers::coordinate_triplet::CoordinateTriplet;
+
+    /// tests `CoordinateTriplet::new()` for success
+    ///
+    /// # Errors
+    /// - `CoordinateTriplet::new()` fails for f64
+    /// - `CoordinateTriplet::new()` fails for u64
+    ///
+    #[test]
+    fn coordinate_triplet_new() {
+        // test f64
+        let a: CoordinateTriplet<f64> = CoordinateTriplet::new(1.0, 2.0, 3.0).unwrap();
+        assert_eq!(CoordinateTriplet {x: 1.0, y: 2.0, z: 3.0}, a);
+        
+        // test u64
+        let b: CoordinateTriplet<u64> = CoordinateTriplet::new(1, 2, 3).unwrap();
+        assert_eq!(CoordinateTriplet {x: 1, y: 2, z: 3}, b);
+    }
 }
