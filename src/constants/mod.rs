23038cbde73862f7da28f9e3c4d5c5a2b91d1f54
Sam Wyss <sjwyss@outlook.com>
2024-11-11 20:52:54 -0500

    added several physical constants

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,3 +1,21 @@
 //! constants module
 //!
 //! container for compile-time constants
+
+/// (F * m^-1) vacuum permittivity https://en.wikipedia.org/wiki/Vacuum_permittivity
+const VAC_PERM: f64 = 8.8541878188e-12;
+
+/// (C) electron charge https://en.wikipedia.org/wiki/Elementary_charge
+const ELEC_CHARGE: f64 = 1.602176634e-19;
+
+/// (kg) atomic mass unit https://en.wikipedia.org/wiki/Dalton_(unit)
+const AMU: f64 = 1.66053906892e-27;
+
+/// (kg) electron mass https://en.wikipedia.org/wiki/Electron_mass
+const ELEC_MASS: f64 = 9.1093837139e-31;
+
+/// (J * K^-1) boltzmann constant https://en.wikipedia.org/wiki/Boltzmann_constant
+const BOLTZMANN: f64 = 1.380649e-23;
+
+/// (K) electron volt temperature
+const EV_TEMP: f64 = ELEC_CHARGE / BOLTZMANN;
